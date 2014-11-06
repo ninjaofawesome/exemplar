d340a3dc5c197f09d6dc10886d82678230f0cc09
Hannah Nordgren <hey@hereshannahs.info>
2014-11-06 17:15:17 -0500

    sidebar directive complete

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,7 +5,17 @@ exemplarDirectives.directive('sideNav', function(){
     restrict: 'E',
     templateUrl: 'app/views/components/sidebar.html',
     controller: function(){
+      $('.hamburger-button').click(function(){
+        $(this).fadeOut(600, function () {
+            $('.side-nav').animate({ left: 0});
+        });
+      });
 
+      $('.close-me').click(function(){
+        $('.side-nav').animate({left: '-=350'}, 600, function(){
+            $('.hamburger-button').fadeIn(600);
+        });
+      });
     }
   }
 });
