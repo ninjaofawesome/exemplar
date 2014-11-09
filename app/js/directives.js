226ff16f86e78bae1bab62c4c5b9278293da643c
Hannah Nordgren <hey@hereshannahs.info>
2014-11-09 14:43:26 -0500

    need to figure out sticky footer, started implementing main page

--- app/js/directives.js
+++ app/js/directives.js
@@ -30,3 +30,18 @@ exemplarDirectives.directive('stickyFooter', function(){
     }
   }
 });
+exemplarDirectives.directive('myBackgroundImage', function () {
+  return function (scope, element, attrs) {
+    element.css({
+      'background-image': 'url(' + attrs.myBackgroundImage + ')',
+      'background-size': 'cover',
+      'background-repeat': 'no-repeat',
+      'background-position': 'center center fixed'
+    });
+  };
+});
+
+
+
+
+
